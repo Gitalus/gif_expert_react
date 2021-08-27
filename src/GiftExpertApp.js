f37394e935d01ce406918796d1bdf5d511302be6
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-26 21:09:12 -0400

    show a proper way to change States of arrays

--- src/GiftExpertApp.js
+++ src/GiftExpertApp.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState} from 'react';
 // GifExpertApp
 
 function GiftExpertApp () {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    function handleAdd() {
+        setCategories(cats => [ 'HxH', ...cats]);
+    }
+
+
     return (
         <>
             <h2>GiftExpertApp</h2>
             <hr />
+
+            <button onClick={handleAdd}>Agregar</button>
+            <ol>
+                {
+                    categories.map(category => <li key={category}>{category}</li>)
+                }
+            </ol>
         </>
     )
 }
