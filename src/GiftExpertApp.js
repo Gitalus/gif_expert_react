6ca11e240ac662229c789139242434168b3baf38
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-26 21:15:18 -0400

    create AddCategory component and import it

--- src/GiftExpertApp.js
+++ src/GiftExpertApp.js
@@ -1,22 +1,21 @@
 import React, { useState} from 'react';
+import { AddCategory } from './components/AddCategory';
 // GifExpertApp
 
 function GiftExpertApp () {
-
-    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
     const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
 
-    function handleAdd() {
-        setCategories(cats => [ 'HxH', ...cats]);
-    }
+    // function handleAdd() {
+    //     setCategories(cats => [ 'HxH', ...cats]);
+    // }
 
 
     return (
         <>
             <h2>GiftExpertApp</h2>
+            <AddCategory />
             <hr />
 
-            <button onClick={handleAdd}>Agregar</button>
             <ol>
                 {
                     categories.map(category => <li key={category}>{category}</li>)
