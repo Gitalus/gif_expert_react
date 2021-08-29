5d3bbd5c7ede36adce7c5202b21c8570f5d7004a
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-28 21:37:07 -0400

    refactor api function to a new file

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
@@ -6,24 +7,9 @@ export const GifGrid = ({ category }) => {
     const [images, setImages] = useState([]);
 
     useEffect(()=>{
-        getGifs();
+        getGifs( category )
+            .then( setImages )
     }, [])
-
-    const getGifs = async () => {
-
-        const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=F4tMfSzlQYzZFM4iS4kQl2O4NxDipcDn`;
-        const resp = await fetch( url );
-        const { data } = await resp.json();
-
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-        setImages( gifs );
-    }
 
     return (
         <>
