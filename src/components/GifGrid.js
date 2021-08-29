2dcb660137fe67318be42a3da583d04cff11b348
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-29 18:57:00 -0400

    add animation using animate.css

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useFetchGifs } from '../hooks/useFetchGifs'
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
@@ -8,19 +8,19 @@ export const GifGrid = ({ category }) => {
 
     return (
         <>
-            <h3> { category }</h3>
+            <h3 className="animate__animated animate__fadeIn animate__delay-2s"> { category }</h3>
 
-            { loading && <p>Loading...</p> }
+            { loading && <p className="animate__animated animate__flash">Loading...</p> }
 
             <div className="card-grid">
-                    {
-                        images.map( img => (
-                            <GifGridItem 
-                            key={ img.id }
-                            { ...img }
-                            />
-                        ))
-                    }
+                {
+                    images.map( img => (
+                        <GifGridItem 
+                        key={ img.id }
+                        { ...img }
+                        />
+                    ))
+                }
             </div>
         </>
     )
